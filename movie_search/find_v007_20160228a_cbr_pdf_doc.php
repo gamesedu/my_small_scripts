8b140ead21e3bd5ec010a350c27ef447ff5d2a4d
mathemecom <matheme.com@gmail.com>
2016-03-01 22:07:20 +0200

    Added files via upload

---
+++ movie_search/find_v007_20160228a_cbr_pdf_doc.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Find movies:
+Search all *.txt files inside this folder and return results
+-v006 :added cbr/cbz/pdf search
+
+*/
+
+		ECHO '
+			<form action="" method="get">
+			  search text: <input type="text" name="s"><br>
+			  <select name="sub_search">
+				<option value="movies" selected>movies (default)</option>
+				<option value="iso">ISO</option>
+				<option value="cbr">CBR comics (NO pdf)</option>
+				<option value="pdf">Documents eg PDF,Doc,PPT and CBR comics</option>
+				<option value="all">all</option>
+
+			</select>
+			<input type="submit" value="Submit">
+			</form>		
+		';	
+		
+		
+$arr_movies=array(".avi",".mpg",".mpeg",".srt",".mp4",".flv",".wmv",".divx",".mkv");
+$arr_iso=array(".iso",".img");	
+$arr_cbr=array(".cbr",".cbz");
+$arr_pdf_cbr=array(".cbr",".cbz",".pdf",".chm",".doc",".ppt");	
+//print_r($arr_movies);	
+$text="0";
+
+
+
+function is_movie($str, array $arr)
+{
+    foreach($arr as $a) {
+        if (stripos($str,$a) !== false) return true;
+    }
+    return false;
+}
+ 
+   
+
+
+if (isSet($_GET["s"])) {
+
+	$text=$_GET["s"];
+		$text = "/^.*$text.*\$/mi";
+		//if (isSet($_GET["sub_search"])&& $_GET["sub_search"]=="movies" ) {$search_text = "/^.*$text.*\$/mi";}
+
+		foreach (glob("*.txt") as $file2search) {
+			echo "<hr size=3><h2>Search inside :$file2search </h2>";
+			$contents = file_get_contents($file2search);
+			//if (!strpos($contents, $text)) continue;
+			//$matches[] = $search;
+			//print_r($matches);
+			
+				if(preg_match_all($text, $contents, $matches)){
+					   echo "Found matches:\n<BR>";
+					   //print_r($matches);
+					   $results= implode("\n<BR>", $matches[0]);
+					   
+					   if (isSet($_GET["sub_search"])&& $_GET["sub_search"]=="movies" ) {
+					   	$sub_matches_array=array();
+					   	foreach($matches[0] as $one_match) {
+					   	//echo"<h1>one_match=$one_match</h1>";
+					   		if(is_movie($one_match,$arr_movies)) array_push($sub_matches_array,$one_match);
+					   		//print_r($sub_matches_array);
+					   		$results=implode("\n<BR>", $sub_matches_array);
+					   	}
+					   }
+					   if (isSet($_GET["sub_search"])&& $_GET["sub_search"]=="iso" ) {
+					   	$sub_matches_array=array();
+					   	foreach($matches[0] as $one_match) {
+					   	//echo"<h1>one_match=$one_match</h1>";
+					   		if(is_movie($one_match,$arr_iso)) array_push($sub_matches_array,$one_match);
+					   		//print_r($sub_matches_array);
+					   		$results=implode("\n<BR>", $sub_matches_array);
+					   	}
+					   }
+					   
+					   if (isSet($_GET["sub_search"])&& $_GET["sub_search"]=="cbr" ) {
+					   	$sub_matches_array=array();
+					   	foreach($matches[0] as $one_match) {
+					   	//echo"<h1>one_match=$one_match</h1>";
+					   		if(is_movie($one_match,$arr_cbr)) array_push($sub_matches_array,$one_match);
+					   		//print_r($sub_matches_array);
+					   		$results=implode("\n<BR>", $sub_matches_array);
+					   	}
+					   }						   
+
+					   if (isSet($_GET["sub_search"])&& $_GET["sub_search"]=="pdf" ) {
+					   	$sub_matches_array=array();
+					   	foreach($matches[0] as $one_match) {
+					   	//echo"<h1>one_match=$one_match</h1>";
+					   		if(is_movie($one_match,$arr_pdf_cbr)) array_push($sub_matches_array,$one_match);
+					   		//print_r($sub_matches_array);
+					   		$results=implode("\n<BR>", $sub_matches_array);
+					   	}
+					   }
+
+					   
+					   echo $results;				   
+				}	   
+				else{
+				   echo "No matches found";
+				}
+
+		}
+	} 
+	else 
+	{
+		echo "hello... no s?";
+
+	}
+
+
+
+
+?>
---
+++ movie_search/readme.md
@@ -0,0 +1,5 @@
+movie_search :
+Check current folder for files *.txt and shows the lines that match the search string.
+Initially made for my movie and comics collections.
+I create my txt file with the command "dir /s /b >harddisk_list.txt".
+I have a txt file for each of my storage media so I can locate in which media what I want is stored.
